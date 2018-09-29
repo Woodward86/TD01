019e92ad8da899c0e32356f497c9159cfc7e92c8
Hero Protagonist <delwoodward86@gmail.com>
2018-09-29 10:37:32 -0700

    basic player controller

---
+++ Assets/PlayerController_Temp.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController_Temp : MonoBehaviour
+{
+
+    protected Rigidbody rb;
+    protected Collider coll;
+
+    public float walkSpeed = 6f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+    }
+
+
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        }
+
+    }
+
+
+
+
+
+}
